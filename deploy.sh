5e0af7dadfc4b4d6414dfc90ece25750c53657f4
truebad0ur <pieceofcakecupofcoffee@gmail.com>
2024-01-19 09:15:56 +0000

    Fix paths

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jekyll build --source /root/mywebsite/jekyll --destination /var/www/ブログ.きく.コム
+jekyll build --source /root/myjekyllblog/jekyll --destination /var/www/ブログ.きく.コム
 nginx -s reload
 
 chown -R www-data:www-data /var/www/ブログ.きく.コム
