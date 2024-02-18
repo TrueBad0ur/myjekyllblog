4434b0edb0d06bd36874605efb3e98ab07d79c02
truebad0ur <pieceofcakecupofcoffee@gmail.com>
2024-02-18 11:41:13 +0000

    Add container

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-jekyll build --source /root/projects/myjekyllblog/jekyll --destination /var/www/ブログ.きく.コム
-nginx -s reload
-
-chown -R www-data:www-data /var/www/ブログ.きく.コム
+jekyll build --source /root/projects/myjekyllblog/jekyll --destination ./static/
